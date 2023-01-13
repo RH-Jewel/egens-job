e6eabfe0f437cac3e9f480861ccd6c975e290fee
Maruf Khan <marufmks5556@gmail.com>
2023-01-13 11:35:34 +0600

    added fix to team

--- inc/ele-widgets/team_widget.php
+++ inc/ele-widgets/team_widget.php
@@ -63,6 +63,23 @@ class Elementor_Team_Widget extends \Elementor\Widget_Base
         );
 
         $repeater->add_control(
+            'egens_team_link',
+            [
+                'label' => esc_html__('Link', 'egenslab'),
+                'type' => \Elementor\Controls_Manager::URL,
+                'placeholder' => esc_html__('https://your-link.com', 'egenslab'),
+                'options' => ['url', 'is_external', 'nofollow'],
+                'default' => [
+                    'url' => 'https://uniflex.recman.no/register.php ',
+                    'is_external' => true,
+                    'nofollow' => true,
+                    // 'custom_attributes' => '',
+                ],
+                'label_block' => true,
+            ]
+        );
+
+        $repeater->add_control(
             'egens_team_member_image',
             [
                 'label' => esc_html__('Image', 'egenslab'),
@@ -119,20 +136,22 @@ class Elementor_Team_Widget extends \Elementor\Widget_Base
             <div class="container">
                 <div class="row">
                     <?php foreach ($data as $item) : ?>
-                        <div class="col-sm-6 col-md-6 col-lg-4 card-wrapper">
-                            <figure class="img-rectangular ratio-container ratio-container-card w-100">
-                                <img width="1600" height="1067" src="<?php echo (!empty($item['egens_team_member_image']['url']) ? esc_url($item['egens_team_member_image']['url']) : '') ?>" class="img-responsive" alt="<?php echo esc_attr__('card-image','egenslab') ?>" >
-                            </figure>
-                            <div class="card-body entry-content">
-                                <h3 class="card-title"><?php echo (!empty($item['egens_team_member_name']) ? esc_html__($item['egens_team_member_name'], 'egenslab') : '') ?></h3>
-                                <p><?php echo (!empty($item['egens_team_member_designation']) ? esc_html__($item['egens_team_member_designation'], 'egenslab') : '') ?></p>
-                            </div>
+                        <div class="col-sm-6 col-md-6 col-lg-4 card-wrapper"> <a href="<?php echo (!empty($item['egens_team_link']['url']) ? esc_url($item['egens_team_link']['url']) : '') ?>" title="Helsingborg" class="card d-flex flex-column" target="_blank">
+                                <figure class="img-rectangular ratio-container ratio-container-card w-100">
+                                    <img width="1600" height="1067" src="<?php echo (!empty($item['egens_team_member_image']['url']) ? esc_url($item['egens_team_member_image']['url']) : '') ?>" class="img-responsive" alt="<?php echo esc_attr__('card-image', 'egenslab') ?>">
+                                </figure>
+                                <div class="card-body entry-content">
+                                    <h3 class="card-title"><?php echo (!empty($item['egens_team_member_name']) ? esc_html__($item['egens_team_member_name'], 'egenslab') : '') ?></h3>
+                                    <p><?php echo (!empty($item['egens_team_member_designation']) ? esc_html__($item['egens_team_member_designation'], 'egenslab') : '') ?></p>
+                                </div>
                             </a>
                         </div>
                     <?php endforeach ?>
                 </div>
             </div>
         </section>
+
+
 
 <?php
     }
