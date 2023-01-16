d417a3fe2fac6d989c8821e13efd738213c0ef39
shuvo247 <mdshakibul.shuvo@gmail.com>
2023-01-16 15:56:33 +0600

    Job archive

---
+++ template-parts/content-archive-jobs.php
@@ -0,0 +1,116 @@
+<?php 
+$curl = curl_init();
+global $wp;
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.recman.no/v2/get/?scope=jobPost&fields=projectId%2C%20name%2C%20title%2C%20ingress%2C%20body%2C%20numberOfPositions%2C%20startDate%2C%20endDate%2C%20logo%2C%20deadline%2C%20departmentId%2C%20facebook%2C%20linkedin%2C%20twitter%2C%20address1%2C%20address2%2C%20postalCode%2C%20city%2C%20country%2C%20web%2C%20salary%2C%20corporationId%2C%20created%2C%20updated%2C%20applyUrl%2C%20contacts%2C%20type%2C%20sector%2C%20accession%2C%20companyName%2C%20workplace%2C%20images%2C%20videoUrl%2C%20branchCategoryId%2C%20branchId%2C%20secondaryBranchCategoryId%2C%20secondaryBranchId%2C%20skills%2C%20countryId%2C%20regionId%2C%20cityId%2C%20position%2C%20positionType%2C%20socialMedia&key=230114013048k7558b612532a34cca8be4a12cbfe60d82021015876",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "postman-token: d845d6ce-7ccc-90f3-1033-bbf7ddc01941"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+}
+
+$response = json_decode( $response );
+// echo "<pre>";
+// print_r( $response );
+// exit();
+function generateExcerpt($string,$length,$end='....')
+{
+    $string = strip_tags($string);
+
+    if (strlen($string) > $length) {
+
+        // truncate string
+        $stringCut = substr($string, 0, $length);
+
+        // make sure it ends in a word so assassinate doesn't become ass...
+        $string = substr($stringCut, 0, strrpos($stringCut, ' ')).$end;
+    }
+    return $string;
+}
+?>
+
+<section class="job-archive" data-page="1">
+	<div class="container js-jobs-container">
+        <div class="row">
+            <div class="col">
+                <div class="d-flex justify-content-between flex-column flex-md-row search-result__wrapper">
+                    <span class="search-result__counter">
+                        Din sökning gav 272 träffar			</span>
+                    <ul class="job-archive__orderby d-flex">
+                        <li>
+                            <input type="radio" name="orderby" value="post_date" id="orderbydate"  checked='checked'>
+                            <label for="orderbydate">
+                                Datum					</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="orderby" value="tax_municipality" id="orderbymunicipality" >
+                            <label for="orderbymunicipality">
+                                Stad</a>
+                            </label>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <?php foreach( $response->data as $jobs ) : ?>
+            <div class="job-post">
+                <div class="row">
+                    <div class="col-xl-2 my-auto">
+                        <div class="align-middle text-xl-center">
+                            <img src="<?php echo $jobs->logo ?? '' ?>" alt="Uniflex Bemanning" />
+                        </div>
+                    </div>
+                    <div class="col-xl-8">
+                        <div class="job-post__date">
+                            <?php echo date("Y-m-d", strtotime($jobs->created)) ?>		
+                        </div>
+                        <a href="<?php echo home_url( $wp->request ). '?jobPostId='.$jobs->jobPostId.'&slug='.strtolower(preg_replace('/[^A-Za-z0-9-]+/', '-', $jobs->title)) ?>" class="job-post__link">
+                            <?php if( !empty( $jobs->title ) ) : ?>
+                                <h2><?php echo $jobs->title ?></h2>
+                            <?php endif ?>
+                        </a>
+
+                        <div class="job-post__specification">
+                            <a href="index9456.html?tax_municipality[]=stockholm">Stockholm</a>
+                            <span class="job-post__delimiter">|</span>						
+                            <a href="index2316.html?tax_timetype[]=deltid">Deltid</a>
+                        </div>
+                        <div class="job-post__description">
+                            <div class="d-none d-xl-block">
+                                <p>
+                                    <?php echo generateExcerpt( $jobs->body,'300' )  ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 d-flex flex-column justify-content-center align-content-start">
+                        <a href="extrajobba-i-dagabs-nya-logistikcenter-i-balsta-61772/index.html" class="btn btn__submit">Läs mer och ansök</a>
+                        <button class="js-save-job btn__save-job" data-save-job="Spara jobb" data-saved-job="Sparat jobb" data-id="53295"><i class="icon-mark-favourite"></i><span>Spara jobb</span></a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach ?>
+        <div class="pagination pagination-footer text-center" data-pages="Array">
+            <span aria-current="page" class="page-numbers current">1</span>
+            <a class="page-numbers" href="page/2/index.html">2</a>
+            <a class="page-numbers" href="page/3/index.html">3</a>
+            <span class="page-numbers dots">&hellip;</span>
+            <a class="page-numbers" href="page/19/index.html">19</a>
+            <a class="next page-numbers" href="page/2/index.html">Nästa sida</a>
+        </div>
+    </div>
+</section>
---
+++ template-parts/content-jobs-single.php
@@ -0,0 +1,147 @@
+<?php 
+
+$slug = get_query_var('slug');
+$jobPostId = get_query_var('job_id');
+
+?>
+
+<section class="single-job-view">
+
+	<header class="single-job-view__header page-hero no-thumbnail">
+		<div class="container">
+			<article class="entry-content text-center text-lg-left no-bottom-border">
+				<div class="single-job-view__date">
+					2023-01-03				</div>
+				<h1>Administratör till Göteborg</h1>
+
+				<div class="single-job-view__button-bar d-flex d-md-block">
+					<button class="js-save-job btn__save-job" data-id="52932">
+						<i class="icon-mark-favourite"></i>Spara jobb					</button>
+					<a class="btn__print-job" href="javascript:window.print();">
+						<i class="icon-print"></i>Skriv ut sida					</a>
+				</div>
+			</article>
+		</div>
+	</header>
+
+	<div class="container">
+
+		<div class="row">
+			<div class="col-12">
+				<div class="row">
+					<div class="col-xl-8 single-job-view__content">
+						<div class="entry-content mx-auto">
+							<p><strong> </strong><br />Just nu söker vi dig som drivs av att få arbeta med administrativa arbetsuppgifter och motiveras över att självständigt få arbeta med problemlösning. Vi letar efter en kommunikativ och lösningsorienterad person som har enkelt för att ta för sig. Som administratör kommer du få arbeta med olika typer av arbetsuppgifter där du själv har utrymme för att hitta egna lösningar.  </p>
+<p>Anställningen kommer vara på heltid där arbetstiderna är dagtid måndag-fredag.</p>
+<p><strong>Vem är du?</strong><br />Du som söker ett uppdrag som administratör ska tidigare ha arbetat med administrativa uppgifter, gärna i samband med service. Då du till stor del kommer använda en dator som arbetsverktyg krävs det att du har god datorvana och är bekväm med olika system och program. I dina arbetsuppgifter kan det förekomma att du kommer arbeta med både engelska och svenska ärenden. Vi ser därav att du är trygg i att tala och skriva på både svenska och engelska. </p>
+<p>Krav: </p>
+<p>•	Fullständiga gymnasiebetyg<br />
+•	Tidigare arbete/utbildning av administration eller service<br />
+•	God datorvana<br />
+•	Skriva och tala flytande svenska<br />
+•	Vara bekväm i engelskt tal och skrift </p>
+						</div>
+					</div>
+					<aside class="col-xl-4 single-job-view__sidebar">
+						<div class="entry-content">
+															<div class="single-job-view__company-logo">
+									<img src="https://cv-uniflex-se.app.intelliplan.eu/JobAd/Logo?jaid=60636" alt="Uniflex Sverige AB" />
+								</div>
+							
+							<div class="single-job-view__submit">
+								<a href="https://jobb.uniflex.se/jobb/60636/ansok/" class="btn btn__submit" target="_blank" rel="noopener noreferrer"> Ansök här </a>
+							</div>
+
+							<h3>Snabbfakta</h3>
+
+							<div class="single-job-view__definitions">
+																																				<div class="single-job-view__definition">
+											<span>Plats:</span>
+
+																							<a href="../index6cc0.html?tax_municipality[]=goteborg">Göteborg</a>
+																					</div>
+																																													<div class="single-job-view__definition">
+											<span>Omfattning:</span>
+
+																							<a href="../index3613.html?tax_timetype[]=heltid">Heltid</a>
+																					</div>
+																																													<div class="single-job-view__definition">
+											<span>Uppdragstyp:</span>
+
+																							<a href="../indexbb09.html?tax_type_of_employment[]=tillsvidareanstallning">Tillsvidareanställning</a>
+																					</div>
+																																													<div class="single-job-view__definition">
+											<span>Befattning:</span>
+
+																							<a href="../indexcf0e.html?tax_job_position[]=administrator">Administratör</a>
+																					</div>
+																	
+																	
+																													<div class="single-job-view__definition">
+											<span>Sista ansökningsdag:</span>
+											<strong>2023-01-17</strong>
+										</div>
+									
+																	
+																													<div class="single-job-view__definition">
+											<span>Referensnummer:</span>
+											<strong>60636</strong>
+										</div>
+									
+																	
+									
+															</div>
+
+							<h3>Tipsa någon om jobbet</h3>
+
+							<div class="single-job-view__definitions">
+								<div class="share-bar">
+									<a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.uniflex.se%2Fjobb%2Fadministrator-till-goteborg-60636%2F"
+										target="_blank" title="Dela på Facebook">
+										<div class="icon-circle">
+											<i class="icon icon-facebook"></i>
+										</div>
+									</a>
+
+									<a href="http://www.twitter.com/share?url=https%3A%2F%2Fwww.uniflex.se%2Fjobb%2Fadministrator-till-goteborg-60636%2F&amp;title=Administrat%c3%b6r%20till%20G%c3%b6teborg"
+										target="_blank" title="Twitter">
+										<div class="icon-circle">
+											<i class="icon icon-twitter"></i>
+										</div>
+									</a>
+
+									<a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=https%3A%2F%2Fwww.uniflex.se%2Fjobb%2Fadministrator-till-goteborg-60636%2F&amp;title=Administrat%c3%b6r%20till%20G%c3%b6teborg"
+										target="_blank" title="Dela på LinkedIn">
+										<div class="icon-circle">
+											<i class="icon icon-linkedin"></i>
+										</div>
+									</a>
+
+									<a href="mailto:?subject=Jag vill tipsa dig om en ledig tjänst hos Uniflex Sverige AB&amp;body=Länk till tjänsten: https://www.uniflex.se/jobb/administrator-till-goteborg-60636/"
+										title="Dela via e-post">
+										<div class="icon-circle">
+											<i class="icon icon-envelope"></i>
+										</div>
+									</a>
+								</div>
+							</div>
+
+																					
+														<h3>Hur får man jobb?</h3>
+
+							<div class="youtube-container">
+								<a href="https://www.youtube.com/embed/Prj7xZ4M9gw" data-featherlight="iframe" data-featherlight-iframe-width="960" data-featherlight-iframe-height="540">
+									<img src="../../../wwwuniflexse.cdn.triggerfish.cloud/uploads/2018/07/uniflex-bemanning-39-300x200.jpg">
+									<div>
+										<i class="icon-triangle-right"></i>
+									</div>
+								</a>
+							</div>
+						</div>
+
+					</aside>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
