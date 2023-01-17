32fc70594155bc04f488a62fa1396ed365af2304
Maruf Khan <marufmks5556@gmail.com>
2023-01-17 15:48:18 +0600

    added fix to icon css

--- functions.php
+++ functions.php
@@ -211,7 +211,6 @@ add_action('widgets_init', 'egenslab_widgets_init');
 function egenslab_scripts()
 {
 	wp_enqueue_style('egns-slick', get_template_directory_uri() . '/assets/css/slick.css', array(), _S_VERSION, 'all');
-	wp_enqueue_style('egns-magnific', get_template_directory_uri() . '/assets/css/magnific-popup.min.css', array(), _S_VERSION, 'all');
 	wp_enqueue_style('egns-classic', get_template_directory_uri() . '/assets/css/classic-themes.min.css', array(), _S_VERSION, 'all');
 	wp_enqueue_style('egns-style', get_template_directory_uri() . '/assets/css/style.css?v=4323', array(), _S_VERSION, 'all');
 	wp_enqueue_style('egns-style-min', get_template_directory_uri() . '/assets/css/style.min.css', array(), _S_VERSION, 'all');
@@ -223,7 +222,6 @@ function egenslab_scripts()
 
 	wp_enqueue_script('egenslab-custom', get_template_directory_uri() . '/assets/js/custom.js', array('jquery'), _S_VERSION, true);
 	wp_enqueue_script('egenslab-navigation', get_template_directory_uri() . '/assets/js/app.min.js', array('jquery'), _S_VERSION, true);
-	wp_enqueue_script('egenslab-magnigfic-popup', get_template_directory_uri() . '/assets/js/jquery.magnific-popup.min.js', array('jquery'), _S_VERSION, true);
 	wp_enqueue_script('egenslab-slick-js', get_template_directory_uri() . '/assets/js/slick.js', array('jquery'), _S_VERSION, true);
 
 
