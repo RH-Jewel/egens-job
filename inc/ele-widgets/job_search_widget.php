2d2865c9d9352b8ff0bbe9a8512c19735b1d1996
Maruf Khan <marufmks5556@gmail.com>
2023-01-17 14:28:17 +0600

    added fix to job serach

--- inc/ele-widgets/job_search_widget.php
+++ inc/ele-widgets/job_search_widget.php
@@ -72,7 +72,7 @@ class Elementor_Job_Search_Widget extends \Elementor\Widget_Base
                             <div class="entry-content">
                                 <h2 class="h2--xl" style="text-align: center;"><?php echo (!empty($settings['egens_job_search_heading_title']) ? esc_html__($settings['egens_job_search_heading_title'], 'egenslab') : '') ?></h2>
                                 <form role="search" method="get" class="search-form" action="https://www.uniflex.se/jobb/">
-                                    <input type="search" name="q" placeholder="Sök på ort, stad eller yrke ..."> <button type="submit"> <i class="icon-search"></i> <span> <b>259</b> jobb </span> </button>
+                                    <input type="search" name="q" placeholder="Sök på ort, stad eller yrke ..."> <button type="submit"> <i class="bi bi-search"></i> <span> <b>259</b> jobb </span> </button>
                                 </form>
                             </div>
                         </div>
