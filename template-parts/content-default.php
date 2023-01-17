cd09181787a0dbc814eabafed638003e4623a257
Maruf Khan <marufmks5556@gmail.com>
2023-01-17 19:35:12 +0600

    added blog fix

--- template-parts/content-default.php
+++ template-parts/content-default.php
@@ -81,87 +81,104 @@ if (is_single()) : ?>
 if (!is_single()) : ?>
 
 
-    <div class="col-12 col-xl-3">
-        <div class="js-posts-filters">
-            <div class="d-xl-none blog-posts-select">
-                <div class="custom-select position-relative is-hidden entry-content"> 
-                    <span class="active-li js-active-li"> 
-                        <span class="js-current-office"> Alla kategorier </span> <i class="bi bi-chevron-down"></i> 
-                    </span>
-                    <ul>
-                        <?php $blog_cat = get_terms('category'); ?>
-
-                        <?php foreach ($blog_cat as $key => $cat) : ?>
-                            <li> <a href="#" data-term-id="<?php echo $cat->slug ?>" class="<?php echo ($key == 0) ? "is-active" : ""; ?>"> <?php echo $cat->name ?> </a> </li>
-                        <?php $key++;
-                        endforeach;
-                        ?>
-                    </ul>
+    <div class="container container-lg">
+        <div class="row">
+            <div class="col-12 md-margin no-margin text-center col-md-10 col-lg-8 medium-column mx-auto">
+                <h1><?php echo esc_html__('Uniflex blogg', 'egenslab') ?></h1>
+                <div class="entry-content mx-auto">
+                    <p><?php echo esc_html__('Hur ser landskapet ut för jobbsökande? Vad trendar inom rekrytering, bemanning och HR? Och hur ser en vanlig dag ut för våra anställda? Klicka runt på vår blogg och lär dig mer om bemanning, rekrytering och framtidens medarbetare.', 'egenslab') ?></p>
                 </div>
-            </div>
-            <div class="posts-filters d-none d-xl-block">
-
-                <?php foreach ($blog_cat as $key => $cat) : ?>
-
-                    <div> <span class="<?php echo ($key == 0) ? "is-active" : ""; ?>" data-term-id="<?php echo $cat->slug ?>"><?php echo $cat->name ?></span>
-                    </div>
-                <?php $key++;
-                endforeach; ?>
-
-
             </div>
         </div>
     </div>
-    <div class="col-12 js-ajax-posts posts-outer-container col-xl-9">
-        <div class="js-spinner text-center d-none">
-            <div class="spinner"></div>
-        </div>
-        <?php foreach ($blog_cat as $key => $value) : ?>
-            <div class="row blog-post-list js-posts-container">
-                <?php
-                $blog_cat = get_posts(
-                    array(
-                        'showposts' => -1, //add -1 if you want to show all posts
-                        'post_type' => 'post',
-                        'tax_query' => array(
-                            array(
-                                'taxonomy' => 'category',
-                                'field' => 'slug',
-                                'terms' => $value->slug //pass your term name here
-                            )
-                        )
-                    )
-                );
-                ?>
 
-                <div class="col-12 col-sm-6">
-                    <div class="blog-post-list__item">
-                        <div class="post--meta">
-                            <?php echo get_the_date('d M, Y'); ?> • <?php echo esc_html(get_the_author()); ?>
+    <section class="component component-blog-posts" data-category="0">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-xl-3">
+                    <div class="js-posts-filters">
+                        <div class="d-xl-none blog-posts-select">
+                            <div class="custom-select position-relative is-hidden entry-content">
+                                <span class="active-li js-active-li">
+                                    <span class="js-current-office"> Alla kategorier </span> <i class="bi bi-chevron-down"></i>
+                                </span>
+                                <ul>
+                                    <?php $blog_cat = get_terms('category'); ?>
+
+                                    <?php foreach ($blog_cat as $key => $cat) : ?>
+                                        <li> <a href="#" data-term-id="<?php echo $cat->slug ?>" class="<?php echo ($key == 0) ? "is-active" : ""; ?>"> <?php echo $cat->name ?> </a> </li>
+                                    <?php $key++;
+                                    endforeach;
+                                    ?>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="entry-content">
-                            <a href="<?php the_permalink() ?>">
-                                <h3 class="black-text-color"><?php the_title() ?></h3>
-                            </a>
-                            <p> <?php
-                                echo substr(get_the_excerpt(), '0', '100');
-                                if (strlen(get_the_excerpt()) > 100) {
-                                    echo '…';
-                                }
-                                ?></p>
-                            <a href="<?php the_permalink() ?>" class="link--arrow"><?php echo esc_html__('Läs mer', 'egenslab') ?></a>
+                        <div class="posts-filters d-none d-xl-block">
+
+                            <?php foreach ($blog_cat as $key => $cat) : ?>
+
+                                <div> <span class="<?php echo ($key == 0) ? "is-active" : ""; ?>" data-term-id="<?php echo $cat->slug ?>"><?php echo $cat->name ?></span>
+                                </div>
+                            <?php $key++;
+                            endforeach; ?>
+
+
                         </div>
                     </div>
                 </div>
+                <div class="col-12 js-ajax-posts posts-outer-container col-xl-9">
+                    <div class="js-spinner text-center d-none">
+                        <div class="spinner"></div>
+                    </div>
+                    <?php foreach ($blog_cat as $key => $value) : ?>
+                        <div class="row blog-post-list js-posts-container">
+                            <?php
+                            $blog_cat = get_posts(
+                                array(
+                                    'showposts' => -1, //add -1 if you want to show all posts
+                                    'post_type' => 'post',
+                                    'tax_query' => array(
+                                        array(
+                                            'taxonomy' => 'category',
+                                            'field' => 'slug',
+                                            'terms' => $value->slug //pass your term name here
+                                        )
+                                    )
+                                )
+                            );
+                            ?>
 
-            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="blog-post-list__item">
+                                    <div class="post--meta">
+                                        <?php echo get_the_date('d M, Y'); ?> • <?php echo esc_html(get_the_author()); ?>
+                                    </div>
+                                    <div class="entry-content">
+                                        <a href="<?php the_permalink() ?>">
+                                            <h3 class="black-text-color"><?php the_title() ?></h3>
+                                        </a>
+                                        <p> <?php
+                                            echo substr(get_the_excerpt(), '0', '100');
+                                            if (strlen(get_the_excerpt()) > 100) {
+                                                echo '…';
+                                            }
+                                            ?></p>
+                                        <a href="<?php the_permalink() ?>" class="link--arrow"><?php echo esc_html__('Läs mer', 'egenslab') ?></a>
+                                    </div>
+                                </div>
+                            </div>
 
-        <?php $key++;
-        endforeach ?>
-        <div class="row js-get-posts-container">
-            <div class="col-12 text-center"> <button class="btn btn--black js-get-posts">Visa fler blogginlägg</button>
+                        </div>
+
+                    <?php $key++;
+                    endforeach ?>
+                    <div class="row js-get-posts-container">
+                        <div class="col-12 text-center"> <button class="btn btn--black js-get-posts"><?php echo esc_html__('Visa fler blogginlägg', 'egenslab') ?></button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 
 <?php endif; ?>
