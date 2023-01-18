427f8a838d3345df378bf12e887ca55de017e28f
Maruf Khan <marufmks5556@gmail.com>
2023-01-18 15:02:27 +0600

    added blog widget  fix

--- inc/ele-widgets/blog_widget.php
+++ inc/ele-widgets/blog_widget.php
@@ -41,7 +41,24 @@ class Elementor_Blog_Widget extends \Elementor\Widget_Base
             ]
         );
 
-
+        $this->add_control(
+            'egens_blog_heading_title',
+            [
+                'label' => esc_html__('Heading Title', 'egenslab'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'default' => esc_html__('Uniflex blogg', 'egenslab'),
+                'label_block' => true,
+            ]
+        );
+        $this->add_control(
+            'egens_blog_heading_description',
+            [
+                'label' => esc_html__('Heading Description', 'egenslab'),
+                'type' => \Elementor\Controls_Manager::TEXTAREA,
+                'default' => esc_html__('Hur ser landskapet ut för jobbsökande? Vad trendar inom rekrytering, bemanning och HR? Och hur ser en vanlig dag ut för våra anställda? Klicka runt på vår blogg och lär dig mer om bemanning, rekrytering och framtidens medarbetare.', 'egenslab'),
+                'label_block' => true,
+            ]
+        );
 
 
         $this->end_controls_section();
@@ -55,6 +72,17 @@ class Elementor_Blog_Widget extends \Elementor\Widget_Base
     {
         $settings = $this->get_settings_for_display();
 ?>
+
+        <div class="container container-lg">
+            <div class="row">
+                <div class="col-12 md-margin no-margin text-center col-md-10 col-lg-8 medium-column mx-auto">
+                    <h1><?php echo (!empty($settings['egens_blog_heading_title']) ? esc_html__($settings['egens_blog_heading_title'], 'egenslab') : '') ?></h1>
+                    <div class="entry-content mx-auto">
+                        <p><?php echo (!empty($settings['egens_blog_heading_description']) ? wp_kses($settings['egens_blog_heading_description'], wp_kses_allowed_html('post')) : '') ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <section class="component component-blog-posts" data-category="0">
             <div class="container">
